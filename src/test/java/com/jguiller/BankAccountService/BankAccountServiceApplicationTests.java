b64f0d189d51b25eff3412b189a656f00988e5a1
JhojanGuille <guille.balvin@gmail.com>
2020-07-13 16:56:31 -0500

    Estructura inicial de BankAccountService

---
+++ src/test/java/com/jguiller/BankAccountService/BankAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jguiller.BankAccountService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
